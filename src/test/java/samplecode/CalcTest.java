4d1db6daf80823941cd39c4274315f50d62fce87
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2017-12-25 15:07:44 +0900

    calc.htmlのテストクラスにコメント追加、コードを簡潔に

--- src/test/java/samplecode/CalcTest.java
+++ src/test/java/samplecode/CalcTest.java
@@ -6,68 +6,69 @@ import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.open;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-
-import com.codeborne.selenide.SelenideElement;
 
 /**
- * 計算ページのテスト
+ * 計算ページのテスト(非同期実行含む)
  */
 public class CalcTest {
 
     @Test
 	public void addTest() {
+    	// 1. テスト対象のURLを開く
     	open("/calc.html");
-		// 数値入力
-		SelenideElement val1Text = $(By.xpath("//input[@id='value1']"));
-		val1Text.val("1");
-		SelenideElement val2Text = $(By.xpath("//input[@id='value2']"));
-		val2Text.val("2");
-		// 加算ボタン押下
+    	// 2. テスト対象の要素(ボタンやテキストボックスなど)を取得する
+    	// 3. テスト対象の要素に対し操作(クリックやキー入力)を行う
+    	// テキストボックスに値入力
+    	$("#value1").val("1");
+    	$("#value2").val("2");
+    	// 加算ボタン押下(同期実行)
 		$(byId("addButton")).click();
-		// アサート
+		// 4. 操作結果のアサーションを行う
 		$(byId("result")).should(value("3"));
 	}
-	
+
 	@Test
 	public void addAsyncFastTest() {
-		open("/calc.html");
-		// 数値入力
-		SelenideElement val1Text = $(By.xpath("//input[@id='value1']"));
-		val1Text.val("1");
-		SelenideElement val2Text = $(By.xpath("//input[@id='value2']"));
-		val2Text.val("2");
-		// 加算ボタン押下
-		$(byId("addAsyncFastButton")).click();
-		// アサート
+    	// 1. テスト対象のURLを開く
+    	open("/calc.html");
+    	// 2. テスト対象の要素(ボタンやテキストボックスなど)を取得する
+    	// 3. テスト対象の要素に対し操作(クリックやキー入力)を行う
+    	// テキストボックスに値入力
+    	$("#value1").val("1");
+    	$("#value2").val("2");
+    	// 加算ボタン押下(非同期実行_4秒以内)
+    	$(byId("addAsyncFastButton")).click();
+		// 4. 操作結果のアサーションを行う
 		$(byId("result")).should(value("3"));
 	}
 
 	@Test
 	public void addAsyncSlowTest() {
-		open("/calc.html");
-		// 数値入力
-		SelenideElement val1Text = $(By.xpath("//input[@id='value1']"));
-		val1Text.val("1");
-		SelenideElement val2Text = $(By.xpath("//input[@id='value2']"));
-		val2Text.val("2");
-		// 加算ボタン押下
-		$(byId("addAsyncSlowButton")).click();
-		// アサート
+    	// 1. テスト対象のURLを開く
+    	open("/calc.html");
+    	// 2. テスト対象の要素(ボタンやテキストボックスなど)を取得する
+    	// 3. テスト対象の要素に対し操作(クリックやキー入力)を行う
+    	// テキストボックスに値入力
+    	$("#value1").val("1");
+    	$("#value2").val("2");
+    	// 加算ボタン押下(非同期実行_5秒かかる処理)
+    	$(byId("addAsyncSlowButton")).click();
+    	// アサート(余裕を持って6秒待つ)
 		$(byId("result")).waitUntil(value("3"),6000);
 	}
 	
 	@Test(expected=AssertionError.class)
 	public void addAsyncSlowTestFail() {
-		open("/calc.html");
-		// 数値入力
-		SelenideElement val1Text = $(By.xpath("//input[@id='value1']"));
-		val1Text.val("1");
-		SelenideElement val2Text = $(By.xpath("//input[@id='value2']"));
-		val2Text.val("2");
-		// 加算ボタン押下
-		$(byId("addAsyncSlowButton")).click();
-		// アサート。デフォルトのタイムアウト待ち時間4秒ではエラーとなる
+		// 1. テスト対象のURLを開く
+    	open("/calc.html");
+    	// 2. テスト対象の要素(ボタンやテキストボックスなど)を取得する
+    	// 3. テスト対象の要素に対し操作(クリックやキー入力)を行う
+    	// テキストボックスに値入力
+    	$("#value1").val("1");
+    	$("#value2").val("2");
+    	// 加算ボタン押下(非同期実行_4秒以上かかる処理)
+    	$(byId("addAsyncSlowButton")).click();
+    	// アサート。デフォルトのタイムアウト待ち時間4秒ではここでエラーとなる
 		$(byId("result")).should(value("3"));
 	}
 }
